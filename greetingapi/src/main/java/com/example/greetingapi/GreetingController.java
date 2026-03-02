272bd36aee6735d125dc913de8df53667554c14c
DhanaM24 <dhananjipallegedara432@gmail.com>
2026-03-02 21:45:27 +0530

    update lab shhet answeer

--- greetingapi/src/main/java/com/example/greetingapi/GreetingController.java
+++ greetingapi/src/main/java/com/example/greetingapi/GreetingController.java
@@ -1,21 +1,29 @@
 package com.example.greetingapi;
-
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// controller for Greeting releated API endpoints
+@RestController
+
 
 public class GreetingController {
+
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();    
 
+    private final AtomicLong count = new AtomicLong();
+
+    // endpoint for responding to calls for /greeting
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }   
-    @GetMapping("/greeting2")
-    public Greeting greeting2(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }   
+    public Greeting greeting() {
+        return new Greeting(count.incrementAndGet(), String.format(template, "World"));
+    }
 
-    
+    // endpoint for responding to calls for /greeting/name?name=<your_name>
+    @GetMapping("/greeting/name")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "<Your name>") String name) {
+        return new Greeting(count.incrementAndGet(), String.format(template, name));
+    }
 }
